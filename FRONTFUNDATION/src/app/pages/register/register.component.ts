cb154c737fbf4873ddafb6340f77c216361b9fab
FundationSemillas <86446293+FundationSemillas@users.noreply.github.com>
2021-07-12 10:49:26 -0500

    logica controlador registrar usuario

--- FRONTFUNDATION/src/app/pages/register/register.component.ts
+++ FRONTFUNDATION/src/app/pages/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AlbumService } from './../../services/album.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -9,10 +13,42 @@ export class RegisterComponent implements OnInit {
   test: Date = new Date();
   focus;
   focus1;
+  registerForm: FormGroup;
+  submitted = false;
 
-  constructor() { }
-
+constructor(private auth: AlbumService, private formBuilder: FormBuilder, private router: Router, private toastr: ToastrService) {
+    this.registerForm = this.formBuilder.group({
+      name: ["", Validators.required],
+      last_name: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", Validators.required],
+      permission: ["true"],
+    });
+  }
+  
   ngOnInit(): void {
   }
 
+  registrarUsuario() {
+    this.submitted = true;
+    if (this.registerForm.invalid) {
+      console.log("error registrae usuario Formulario")
+      return;
+    }
+    console.log("objeto usuario: ", this.registerForm.value);
+    this.auth.add(this.registerForm.value, "/register").subscribe(
+      res => {
+        console.log("creado exitosamente");
+      },
+      err => {
+        console.log("error crear", err)
+      }
+
+    );
+    this.toastr.success('Usuario Registrado Exitosamente');
+    this.registerForm.reset();
+    this.registerForm.clearValidators();
+    this.registerForm.clearAsyncValidators();
+  }
+  
 }
