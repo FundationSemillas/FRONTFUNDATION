0fdc4b660ea5ae4f277e1bad0e3885a7bdad540c
gibranjulyano11 <gibranjulyano11@gmail.com>
2021-10-15 09:13:56 -0500

    cambios

--- FRONTFUNDATION/src/app/components/navbar/navbar.component.ts
+++ FRONTFUNDATION/src/app/components/navbar/navbar.component.ts
@@ -5,7 +5,9 @@ import { Location, LocationStrategy, PathLocationStrategy } from '@angular/commo
 @Component({
     selector: 'app-navbar',
     templateUrl: './navbar.component.html',
-    styleUrls: ['./navbar.component.scss']
+    // styleUrls: ['./navbar.component.scss'] ,
+    // styleUrls: ['./navbar.component.css']
+
 })
 export class NavbarComponent implements OnInit {
     private toggleButton: any;
